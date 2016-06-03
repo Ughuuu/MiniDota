926533dda9b1b5162e850d5d4b77bfaa4ea05c9f
ughuuu <daian.dragos@yahoo.com>
2016-06-03 16:38:12 +0300

    updated with demo

---
+++ game.js
@@ -0,0 +1,125 @@
+// You can use either `new PIXI.WebGLRenderer`, `new PIXI.CanvasRenderer`, or `PIXI.autoDetectRenderer`
+// which will try to choose the best renderer for the environment you are in.
+var renderer = new PIXI.autoDetectRenderer(1024, 700, {transparent: true});
+
+// The renderer will create a canvas element for you that you can then insert into the DOM.
+$('#canvasHolder').append(renderer.view);
+
+// You need to create a root container that will hold the scene you want to draw.
+var stage = new PIXI.Container();
+var PLAYERS = [];
+var MAX_PLAYERS = 100;
+var BIG_NR = -100000;
+
+var camera = new PIXI.Container();
+camera.scale.x = 0.5;
+camera.scale.y = 0.5;
+camera.position.x = 500;
+camera.position.y = 350;
+
+var graphics = new PIXI.Graphics();
+ 
+// Set the fill color
+graphics.beginFill(0xe74c3c); // Red
+ 
+// Draw a circle
+graphics.drawCircle(0, 0, 700); // drawCircle(x, y, radius)
+ 
+// Applies fill to lines and shapes since the last call to beginFill.
+graphics.endFill();
+  
+// Add the graphics to the stage
+camera.addChild(graphics);
+
+// load the texture we need
+PIXI.loader.add('gear', 'gear2.png').load(function (loader, resources) {
+    
+    for (var i = 0; i < MAX_PLAYERS; i++) {
+        var player = new PIXI.Sprite(resources.gear.texture);
+        var text = new PIXI.Text("Heya!", {font:"30px Arial", fill:"black"});
+
+        text.position.y = -50;
+
+        var playerContainer = new PIXI.Container();
+        // Setup the position and scale of the bunny
+
+        player.anchor.x = 0.5;
+        player.anchor.y = 0.5;
+
+        player.scale.x = 1;
+        player.scale.y = 1;
+        // Add the bunny to the scene we are building.
+        playerContainer.addChild(text);
+        playerContainer.addChild(player); 
+
+        PLAYERS.push(playerContainer);
+
+        camera.addChild(playerContainer)
+    }
+
+    for(var i = 0; i< PLAYERS.length; i++){
+        PLAYERS[i].position.x = BIG_NR;
+        PLAYERS[i].position.y = BIG_NR;        
+    }
+    // kick off the animation loop (defined below)
+    animate();
+});       
+   
+stage.addChild(camera);
+
+var socket = io();
+
+$(window).on('beforeunload', function(){
+    socket.close();
+});
+
+function doConn(){      
+      var nameCurrent = $('#send').val();
+      socket.emit('makePlayer',{name:nameCurrent});
+      $('#send').val('');
+      $('#hellomsg').text(nameCurrent);
+      $('#button').text('Send');
+}
+
+function rgbToHex(r, g, b) {
+    return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function getColor(name){
+    var col = 0;
+    for(var i=0; i<name.length; i++){
+        col+=name[i].charCodeAt(0);
+    }
+    col*=1234;
+    col%=16581375;
+    return col;
+}
+
+
+socket.on('gameEvent',function(data){
+    var n = data.length;
+    if(PLAYERS.length < n){
+        n = PLAYERS.length;
+    }
+    for(var i = 0 ; i < n; i++){
+        PLAYERS[i].position.x = data[i].x;
+        PLAYERS[i].position.y = -data[i].y;
+        PLAYERS[i].getChildAt(1).rotation = data[i].ang;
+        PLAYERS[i].getChildAt(0).text = data[i].name;
+        PLAYERS[i].getChildAt(0).position.x = -data[i].name.length*8;
+        PLAYERS[i].getChildAt(1).tint = getColor(data[i].name);
+    console.log(data[i].name);
+    }
+});
+
+socket.on('chat',function(chat){
+    $('#messages').append("<li>" + chat + "</li>");
+});
+
+function animate() {
+    // start the timer for the next animation loop
+    requestAnimationFrame(animate);
+
+    // this is the main render call that makes pixi draw your container and its children.
+    renderer.render(stage);
+}
---
+++ index.js
@@ -0,0 +1,119 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/pages/index.html');
+});
+
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+var NEW_PLAYER_LIST = [];
+var GLOBAL_ID = 0;
+var PLAYERS = 0;
+var timeStep = 1000 / 60.0;
+
+io.on('connection', function(socket){
+
+	socket.id = GLOBAL_ID + 1;
+		GLOBAL_ID++;
+	SOCKET_LIST[socket.id] = socket;
+
+	socket.on('makePlayer',function(name){
+    	if(PLAYER_LIST[socket.id] != null)
+    		return;
+		PLAYERS++;
+	 	NEW_PLAYER_LIST.push({id:socket.id, name:name.name});
+    });
+
+	socket.on('chat message', function(msg){
+	  	io.emit('chat message', msg);
+		console.log("Name: "+ name + " says " + msg);
+	});
+
+	socket.on('disconnect',function(){
+	    delete SOCKET_LIST[socket.id];
+    	if(PLAYER_LIST[socket.id] == null)
+    		return;
+    	PLAYER_LIST[socket.id].destroy = true;
+		PLAYERS--;
+    });
+});
+
+function connectNewPlayers(){	
+	for (var i = NEW_PLAYER_LIST.length - 1; i >= 0; i--) {
+		var id = NEW_PLAYER_LIST[i].id;
+		var name = NEW_PLAYER_LIST[i].name;
+		if(PLAYER_LIST[id] != null){
+			continue;
+		}
+	    var player = Player(id, name);
+	    PLAYER_LIST[id] = player;
+	}
+	NEW_PLAYER_LIST = [];
+}
+
+function removeDisconnected(){	
+    for(var i in PLAYER_LIST){
+        var player = PLAYER_LIST[i];
+        if(player.destroy == true){    
+        	PLAYERS--;    	
+			
+        	//destroy player here
+
+	        delete PLAYER_LIST[player.id];
+        }
+    }
+}
+
+function computeData(){	
+    var gameData = [];
+    for(var i in PLAYER_LIST){
+        var player = PLAYER_LIST[i];
+
+        gameData.push({
+
+        	//send relevant data here
+
+            x:200,
+            y:300,
+            ang:3,
+
+            name:player.name
+        });    
+    }
+    return gameData;
+}
+
+function sendData(data){	
+    for(var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i];
+        socket.emit('gameEvent', data);
+    }
+}
+
+function updatePlayers(){
+	connectNewPlayers();
+
+	removeDisconnected();
+
+	var gameData = computeData();
+
+	sendData(gameData);
+}
+
+setInterval(updatePlayers, timeStep);
+
+var Player = function(id, name){
+    var self = {
+    	name:name,
+        id:id,
+    }
+    return self;
+}
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
